6d6c94599cf3c1460b7845a11a7a2893420f891a
Manoel Costa <43146346+AelmonCipher@users.noreply.github.com>
2018-10-24 16:09:27 -0300

    Create ex001.py

---
+++ Exercicios_CursoEmVideo/ex001.py
@@ -0,0 +1,4 @@
+# Fazer um programa que leia o nome e cumprimente a pessoa usando o nome dela
+print('{} DESAFIO 02 {}'.format(('=*'5), ('='*5)))
+nome = input('Digite o seu nome: ')
+print('Estou feliz em conhece-lo, {}'.format(nome))
